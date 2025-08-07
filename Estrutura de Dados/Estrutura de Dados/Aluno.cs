64281ba3d4585164bb397e134444f3cdf06b24fc
Nelson Dos Reis Gomes Souza <nelsondosreisgomessouza@gmail.com>
2025-08-06 22:01:49 -0300

    wip adicionar projeto

---
+++ Estrutura de Dados/Estrutura de Dados/Aluno.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_Dados
+{
+    internal class Aluno
+    {
+    }
+}
